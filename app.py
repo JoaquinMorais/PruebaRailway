395f796f6b26f4644fd41a671c7a050d29c556cf
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-02-15 23:34:21 -0300

    ay diosito mio

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:i3eXbNRZa4GIGFN37K8s@containers-us-west-151.railway.app:5647/railway'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(70), unique=True)
+    description = db.Column(db.String(100))
+
+    def __init__(self,title,description):
+        self.title = title
+        self.description = description
+
+db.create_all()
+
+@app.route('/')
+def home():
+    return 'Oremos dios mio aaaa'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
