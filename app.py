38465aa0aa72e4859f139eef23ba3ea347f8ee6f
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-02-17 19:09:07 -0300

    prueba n8 ma o me

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from create_app import app
 from utils.db import db
+from models.tasks import Task
 
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def home():
-    return 'holis'
+    if Task.query.all():
+        newInstance = Task(f'TareaPiola{Task.query.order_by(Task.id.desc()).first().id}','jijijajajajajajaj')
+    else:
+        newInstance = Task(f'TareaPiolaOriginal','fui primero jijijija')
+    db.session.add(newInstance)
+    db.session.commit()
+    return f'Oremos dios mio aaaa {Task.query.all()}'
 
 if __name__ == '__main__':
     app.run(debug=True)
