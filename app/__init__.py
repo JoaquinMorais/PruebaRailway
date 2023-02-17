d0f75fb6a4c167e9044a95624901315e7828fbd5
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-02-17 18:21:53 -0300

    prueba n4

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from create_app import app
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello():
-    return 'Hello World 2!'
+    return 'Hello World 3!'
 
 if __name__ == '__main__':
     app.run(debug=True)
