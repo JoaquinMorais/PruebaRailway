caa7fe49c299dc2965223d8493c428f3c498947b
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-02-17 18:42:23 -0300

    prueba n5 utils.db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from create_app import app
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+from utils.db import db, ma
 
 with app.app_context():
     db.create_all()
